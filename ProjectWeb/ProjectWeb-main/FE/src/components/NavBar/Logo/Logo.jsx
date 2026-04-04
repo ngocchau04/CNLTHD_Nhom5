940eed90e1c84c7f76dcc0f7063e8bf9c471c1ec
ngocchau04 <damthingocchau26@gmail.com>
2026-04-04 14:44:42 +0700

    delete projectweb

--- ProjectWeb/ProjectWeb-main/FE/src/components/NavBar/Logo/Logo.jsx
+++
@@ -1,6 +0,0 @@
-import logo from '../../../assets/images/logo.png';
-import "./Logo.css";
-const Logo = () => {
-  return <img src={logo} alt="logo" className="logo-img" />
-};
-export default Logo;
